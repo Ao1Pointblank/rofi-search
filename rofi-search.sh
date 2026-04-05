575f1861d37da35b22ff450eecb8835cab91063c
Ao1Pointblank <88149675+Ao1Pointblank@users.noreply.github.com>
2026-04-05 16:37:49 -0400

    changed default browser, added engine icons, changed rofi-dict to hunspell & dict

--- rofi-search.sh
+++ rofi-search.sh
@@ -3,38 +3,46 @@
 
 ENGINES_DIR=~/.local/opt/rofi-search-engines/
 ENGINE_DEFAULT=Brave
-PRIVATE_MODE_TEXT="Press ENTER to use private Mullvad window, or type query for normal search"
+PRIVATE_MODE_TEXT="Press ENTER to use private browser window, or type query for normal search"
 
-DEFAULT_BROWSER="firefox"
-DEFAULT_PRIVATE_BROWSER="firefox --private-window"
+DEFAULT_BROWSER="flatpak run app.zen_browser.zen"
+DEFAULT_PRIVATE_BROWSER="flatpak run app.zen_browser.zen --private-window"
 
-ENGINE_SELECT=$(ls $ENGINES_DIR | rofi -dmenu -p "Search" -i -selected-row 1 -l $(ls -1 $ENGINES_DIR | wc -l)) &&
+#added icon support for engines. store icons as svg in $ENGINES_DIR/.ICONS/name_of_engine.svg
+ENGINE_SELECT=$(ls $ENGINES_DIR | while read A ; do  echo -en "$A\x00icon\x1f$ENGINES_DIR/.ICONS/$A.svg\n"  ; done | rofi -dmenu -p "Search" -i -selected-row 2 -l $(ls -1 $ENGINES_DIR | wc -l)) &&
+
 if [[ -e $ENGINES_DIR"$ENGINE_SELECT" ]]
 then
 
     if [[ $ENGINE_SELECT == "Firefox Bookmarks" ]]
     then
 		#make sure the delimiter (𝆓 by default) matches the one in firefox_bookmarks.py
-    	QUERY=$(python3 "$ENGINES_DIR"/"Firefox Bookmarks" | rofi -dmenu -p "Firefox Bookmarks" -i | awk -F"𝆓" '{print $2}') &&
-        $DEFAULT_BROWSER $QUERY
+    	QUERY=$(python3 "$ENGINES_DIR"/"Firefox Bookmarks" | rofi -dmenu -p "Firefox Bookmarks" -i | awk -F"𝆓" '{print $2}')
+        if [[ -n "$QUERY" ]]; then
+            $DEFAULT_BROWSER $QUERY
+        fi
+
     elif [[ $ENGINE_SELECT == "Freetube" ]]
     then
     	QUERY=$(rofi -dmenu -p "Search Freetube" -i)
         "$ENGINES_DIR"/Freetube --rofi "$QUERY"
-    elif [[ $ENGINE_SELECT == "Dictionary (Local)" ]]
+    elif [[ $ENGINE_SELECT == "Spellcheck" ]]
     then
-    	rofi-dict () {
-    		QUERY=$(rofi -dmenu -lines 0 -p 'Search Dictionary') && dict "$QUERY" | rofi -dmenu -fullscreen -p "Dictionary results for '$QUERY'" && rofi-dict
-    	}
-    	rofi-dict
+        QUERY=$(rofi -dmenu -lines 0 -p 'Search Dictionary')
+        WORD=$(echo "$QUERY" | hunspell -a | awk '/^&/{for(i=5;i<=NF;i++) {gsub(/,$/, "", $i); print $i}}')
+        if [[ -z "$WORD" ]]
+        then
+            dict "$QUERY" | rofi -dmenu -fullscreen -p "Dictionary results for '$QUERY'"
+        else
+            echo "$WORD" | rofi -dmenu -p "Spellcheck results for '$QUERY'" | xclip -selection clipboard
+        fi
     else
         QUERY=$(echo $PRIVATE_MODE_TEXT | rofi -l 1 -dmenu -p "$ENGINE_SELECT Search") &&
 
 	    if [[ $QUERY == $PRIVATE_MODE_TEXT ]]
 	    then
             PRIVATE_QUERY=$(rofi -lines 0 -dmenu -p "Private $ENGINE_SELECT Search") &&
-		$(cat $ENGINES_DIR"$ENGINE_SELECT")"$PRIVATE_QUERY"
-    	$DEFAULT_PRIVATE_BROWSER $(cat $ENGINES_DIR"$ENGINE_SELECT")"$PRIVATE_QUERY"
+    		$DEFAULT_PRIVATE_BROWSER $(cat $ENGINES_DIR"$ENGINE_SELECT")"$PRIVATE_QUERY"
     	else
 		    $DEFAULT_BROWSER $(cat $ENGINES_DIR"$ENGINE_SELECT")"$QUERY"
 	    fi
